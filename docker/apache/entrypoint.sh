9d668f75175d2dd3a7ead10d49de56f1332b3d4b
mtaku3 <m.taku3.1222@gmail.com>
2023-05-02 14:01:11 +0000

    feat: install Flowbite

--- docker/apache/entrypoint.sh
+++ docker/apache/entrypoint.sh
@@ -6,4 +6,6 @@ npm install -D
 
 ./build.sh
 
+ln ./node_modules/flowbite/dist/flowbite.min.js ./dist/
+
 apache2ctl -D FOREGROUND
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{twig,js,php}"],
+  content: ["./src/**/*.{twig,js,php}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
 
