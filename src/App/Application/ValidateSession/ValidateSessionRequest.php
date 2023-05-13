998336d1339f702fdf3854c2547ddaff38bdc601
mtaku3 <m.taku3.1222@gmail.com>
2023-05-13 06:08:23 +0000

    fix: fix request of ValidateSessionUseCase

--- src/App/Application/ValidateSession/ValidateSessionRequest.php
+++ src/App/Application/ValidateSession/ValidateSessionRequest.php
@@ -4,23 +4,21 @@ declare(strict_types=1);
 
 namespace App\Application\ValidateSession;
 
-use App\Application\Session\ValueObject\AccessToken;
-use App\Application\Session\ValueObject\RefreshToken;
 use App\Domain\User\ValueObject\UserId;
 
 class ValidateSessionRequest
 {
     public readonly UserId $UserId;
-    public readonly AccessToken $AccessToken;
-    public readonly RefreshToken $RefreshToken;
+    public readonly string $AccessToken;
+    public readonly string $RefreshToken;
 
     /**
      * @param UserId $userId
-     * @param AccessToken $accessToken
-     * @param RefreshToken $refreshToken
+     * @param string $accessToken
+     * @param string $refreshToken
      * @return void
      */
-    public function __construct(UserId $userId, AccessToken $accessToken, RefreshToken $refreshToken)
+    public function __construct(UserId $userId, string $accessToken, string $refreshToken)
     {
         $this->UserId = $userId;
         $this->AccessToken = $accessToken;
