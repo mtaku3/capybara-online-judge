db13fb1c46cf3679e602a32d7a9bb6b86a244e5d
mtaku3 <m.taku3.1222@gmail.com>
2023-05-13 06:17:05 +0000

    fix: fix response of ValidateSessionUseCase

--- src/App/Application/ValidateSession/ValidateSessionResponse.php
+++ src/App/Application/ValidateSession/ValidateSessionResponse.php
@@ -4,30 +4,22 @@ declare(strict_types=1);
 
 namespace App\Application\ValidateSession;
 
+use App\Application\Session\Entity\Session;
 use App\Application\Session\ValueObject\AccessToken;
-use App\Application\Session\ValueObject\RefreshToken;
-use InvalidArgumentException;
 
 class ValidateSessionResponse
 {
-    public readonly bool $IsValid;
-    public readonly ?AccessToken $AccessToken;
-    public readonly ?RefreshToken $RefreshToken;
+    public readonly AccessToken $AccessToken;
+    public readonly ?Session $Session;
 
     /**
-     * @param null|AccessToken $accessToken
-     * @param null|RefreshToken $refreshToken
+     * @param AccessToken $accessToken
+     * @param null|Session $session
      * @return void
-     * @throws InvalidArgumentException
      */
-    public function __construct(?AccessToken $accessToken, ?RefreshToken $refreshToken)
+    public function __construct(AccessToken $accessToken, ?Session $session)
     {
-        if ((isset($accessToken) && !isset($refreshToken)) || (!isset($accessToken) && isset($refreshToken))) {
-            throw new InvalidArgumentException();
-        }
-
-        $this->IsValid = isset($accessToken) && isset($refreshToken);
         $this->AccessToken = $accessToken;
-        $this->RefreshToken = $refreshToken;
+        $this->Session = $session;
     }
 }
