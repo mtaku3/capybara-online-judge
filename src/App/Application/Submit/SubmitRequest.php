0164128228cfd2816426f5c934def326aa49f4e2
mtaku3 <m.taku3.1222@gmail.com>
2023-05-15 03:25:08 +0000

    fix: fix request of SubmitUseCase

--- src/App/Application/Submit/SubmitRequest.php
+++ src/App/Application/Submit/SubmitRequest.php
@@ -5,9 +5,19 @@ declare(strict_types=1);
 namespace App\Application\Submit;
 
 use App\Domain\Common\ValueObject\Language;
+use App\Domain\Problem\ValueObject\ProblemId;
+use App\Domain\User\ValueObject\UserId;
 
 class SubmitRequest
 {
+    /**
+     * @var UserId
+     */
+    public readonly UserId $UserId;
+    /**
+     * @var ProblemId
+     */
+    public readonly ProblemId $ProblemId;
     /**
      * @var Language
      */
@@ -18,12 +28,16 @@ class SubmitRequest
     public readonly int $CodeLength;
 
     /**
+     * @param UserId $userId
+     * @param ProblemId $problemId
      * @param Language $language
      * @param int $codeLength
      * @return void
      */
-    public function __construct(Language $language, int $codeLength)
+    public function __construct(UserId $userId, ProblemId $problemId, Language $language, int $codeLength)
     {
+        $this->UserId = $userId;
+        $this->ProblemId = $problemId;
         $this->Language = $language;
         $this->CodeLength = $codeLength;
     }
