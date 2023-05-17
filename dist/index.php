27de0eb9b2751bd012676af89aa6a16d3bad269d
mtaku3 <m.taku3.1222@gmail.com>
2023-05-17 10:20:10 +0000

    feat: set cookie at the session validation

--- dist/index.php
+++ dist/index.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Application\Authorize\AuthorizeRequest;
 use App\Application\ValidateSession\ValidateSessionRequest;
 use App\Domain\User\ValueObject\UserId;
 use App\Presentation\Router\AbstractResponse;
@@ -28,17 +29,29 @@ $router->onHttpError(function (int $code, Router $router) use ($container) {
     }
 });
 
-$router->respond(function (Request $req) use ($container) {
+$router->respond(function (Request $req, AbstractResponse $res) use ($container) {
     $cookies = $req->cookies();
 
     if (isset($cookies["x-user-id"]) && isset($cookies["x-refresh-token"])) {
         try {
+            /** @var \App\Application\ValidateSession\ValidateSessionResponse */
             $validateSessionResponse = $container->get("ValidateSessionUseCase")->handle(
                 new ValidateSessionRequest(new UserId($cookies["x-user-id"]), $cookies["x-access-token"], $cookies["x-refresh-token"])
             );
+
+            $res->cookie("x-user-id", (string)$validateSessionResponse->User->getId(), ($validateSessionResponse->Session ?? $validateSessionResponse->AccessToken)->getExpiresAt()->getTimestamp(), secure: $_ENV["ISDEV"] ? false : true, httponly: true, samesite: "strict");
+            $res->cookie("x-access-token", (string)$validateSessionResponse->AccessToken, $validateSessionResponse->AccessToken->getExpiresAt()->getTimestamp(), secure: $_ENV["ISDEV"] ? false : true, httponly: true, samesite: "strict");
+            if (empty($validateSessionResponse->Session)) {
+                $res->cookie("x-refresh-token", expiry: 1);
+            } else {
+                $res->cookie("x-refresh-token", (string)$validateSessionResponse->Session->getRefreshToken(), $validateSessionResponse->Session->getExpiresAt()->getTimestamp(), secure: $_ENV["ISDEV"] ? false : true, httponly: true, samesite: "strict");
+            }
+
             $req->user = $validateSessionResponse->User;
         } catch (Exception $e) {
-            // ignored
+            $res->cookie("x-user-id", expiry: 1);
+            $res->cookie("x-access-token", expiry: 1);
+            $res->cookie("x-refresh-token", expiry: 1);
         }
     }
 });
