eec227f6b59cbff5200c00eba8077beda8fcd21c
mtaku3 <m.taku3.1222@gmail.com>
2023-05-18 02:26:43 +0000

    feat: provisional implementation

--- src/App/Presentation/Controller/ProblemListController.php
+++ src/App/Presentation/Controller/ProblemListController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Presentation\Controller;
 
+use App\Domain\Problem\Entity\Problem;
+use App\Domain\Problem\ValueObject\ProblemId;
 use App\Presentation\Router\AbstractResponse;
 use App\Presentation\Router\Request;
 use Twig\Environment;
@@ -27,7 +29,15 @@ class ProblemListController
     public function get(Request $req, AbstractResponse $res)
     {
         $res->body($this->Twig->render("ProblemList.twig", [
-            "user" => $req->user
+            "user" => $req->user,
+            "problems" => [
+                new Problem(ProblemId::NextIdentity(), "This is a problem 1", "Test string for body", 1000, 1000, array(), array()),
+                new Problem(ProblemId::NextIdentity(), "This is a problem 2", "Test string for body", 1000, 1000, array(), array()),
+                new Problem(ProblemId::NextIdentity(), "This is a problem 3", "Test string for body", 1000, 1000, array(), array()),
+                new Problem(ProblemId::NextIdentity(), "This is a problem 4", "Test string for body", 1000, 1000, array(), array()),
+                new Problem(ProblemId::NextIdentity(), "This is a problem 5", "Test string for body", 1000, 1000, array(), array()),
+                new Problem(ProblemId::NextIdentity(), "This is a problem 6", "Test string for body", 1000, 1000, array(), array()),
+            ]
         ]))->send();
     }
 }
