10ca9c26f993d73c427ff6eb139a1fcad64583ce
aobuto12345 <aobuto2225@gmail.com>
2023-07-03 16:04:06 +0900

    'あとテストケースしたの制約のみ'

--- tests/unit/problemTest.php
+++ tests/unit/problemTest.php
@@ -69,17 +69,73 @@ class ProblemTest extends TestCase
         }
     }
 
-    public function test_problem_has_disableFlag()
+    public function test_does_one_language_have_just_one_compileRule()
     {
-        $this->expectNotToPerformAssertions();
+        $this->expectException(InvalidDTOException::class);
+
         $langueageC = Language::C;
-        $compileRuleDTOs = array(new CompileRuleFactoryDTO($langueageC, 'rightComand', 'rightComand'));
+        // １つの言語につき、2つのcompileRuleがあるため例外が投げられる
+        $compileRuleDTOs = array(
+            new CompileRuleFactoryDTO($langueageC, 'rightComand1', 'rightComand1'),
+            new CompileRuleFactoryDTO($langueageC, 'rightComand2', 'rightComand2')
+        );
+
         $ExecutionRuleDTOs = array(new ExecutionRuleFactoryDTO($langueageC, 'right', 'right', 'right', 'right'));
         $testCaseDTOs = array(new TestCaseFactoryDTO('rightComand', $ExecutionRuleDTOs));
         $problem = Problem::Create('rightTitle', 'rightBoby', 1, 200, $compileRuleDTOs, $testCaseDTOs);
-        $testCases = $problem->getTestCases();
-        foreach ($testCases as $testCase) {
-            $testCase->_disable();
-        }
+        
+    }
+    public function test_does_one_language_have_just_one_executionRule()
+    {
+        $this->expectException(InvalidDTOException::class);
+        $langueageC = Language::C;
+        $langueageCPP = Language::CPP;
+        $compileRuleDTOs = array(new CompileRuleFactoryDTO($langueageC, 'rightComand1', 'rightComand1'));
+        // １つの言語につき、2つのExecutionRuleがあるため例外が投げられる
+        $ExecutionRuleDTOs = array(
+            new ExecutionRuleFactoryDTO($langueageC, 'right1', 'right1', 'right1', 'right1'),
+            new ExecutionRuleFactoryDTO($langueageC, 'right2', 'right2', 'right2', 'right2'),
+        );
+        $testCaseDTOs = array(new TestCaseFactoryDTO('rightComand', $ExecutionRuleDTOs));
+        $problem = Problem::Create('rightTitle', 'rightBoby', 1, 200, $compileRuleDTOs, $testCaseDTOs);
+    }
+
+
+    public function test_problm_atLeastOneEnabledTestCase_whenCeate()
+    {
+        $this->expectException(AtLeastOneEnabledTestCaseRequiredException::class);
+        $langueageC = Language::C;
+        $compileRuleDTOs = array(new CompileRuleFactoryDTO($langueageC, 'rightComand1', 'rightComand1'));
+        $testCaseDTOs = array();
+        $problem = Problem::Create('rightTitle', 'rightBoby', 1, 200, $compileRuleDTOs, $testCaseDTOs);
+    }
+
+    // public function test_problm_atLeastOneEnabledTestCase_whenDisable()
+    // {
+    //     $this->expectException(AtLeastOneEnabledTestCaseRequiredException::class);
+    //     $langueageC = Language::C;
+    //     $compileRuleDTOs = array(new CompileRuleFactoryDTO($langueageC, 'rightComand1', 'rightComand1'));
+    //     $testCaseDTOs = array();
+    //     $problem = Problem::Create('rightTitle', 'rightBoby', 1, 200, $compileRuleDTOs, $testCaseDTOs);
+    // }
+
+    public function test_allSubmittableLanguage_excutionRule()
+    {
+        $this->expectException(InvalidDTOException::class);
+        $langueageC = Language::C;
+        $langueageCPP = Language::CPP;
+        // 提出可能な言語をcとcppにしている
+        $compileRuleDTOs = array(
+            new CompileRuleFactoryDTO($langueageC, 'rightComand1', 'rightComand1'),
+            new CompileRuleFactoryDTO($langueageCPP, 'rightComand1', 'rightComand1'),
+
+        );
+
+        // 提出可能な言語がcとcppの2つであるが、cにしかExecutionRuleがないため例外が投げられる.
+        $ExecutionRuleDTOs = array(
+            new ExecutionRuleFactoryDTO($langueageC, 'right1', 'right1', 'right1', 'right1'),
+        );
+        $testCaseDTOs = array();
+        $problem = Problem::Create('rightTitle', 'rightBoby', 1, 200, $compileRuleDTOs, $testCaseDTOs);
     }
 }
