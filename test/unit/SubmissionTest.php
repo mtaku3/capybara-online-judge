15387c11b87868a8474b4622c8ec63d9e0e2983f
IwamotoKazuma <kazuma384267@icloud.com>
2023-07-03 05:38:43 +0000

    test: SubmissionTestの実装

--- test/unit/SubmissionTest.php
+++ test/unit/SubmissionTest.php
@@ -1,81 +1,171 @@
 <?php
 
-declare(strict_type=1);
+declare(strict_types=1);
 
 use App\Domain\Common\ValueObject\Language;
-use App\Domain\Problem\Entity\ExecutionRule;
 use App\Domain\User\Entity\User;
 use App\Domain\Problem\Entity\Problem;
 use PHPUnit\Framework\TestCase;
-use App\Domain\Submission\Entity\SourceFile;
 use App\Domain\Submission\Entity\Submission;
-use App\Domain\Submission\Entity\TestResult;
-use App\Domain\Submission\Exception\AlreadyJudgedException;
-use App\Domain\Submission\Exception\TestResultForGivenTestCaseAlreadyExistsException;
 use App\Domain\Problem\Factory\ExecutionRuleFactoryDTO;
 use App\Domain\Problem\Factory\CompileRuleFactoryDTO;
 use App\Domain\Problem\Factory\TestCaseFactoryDTO;
+use App\Domain\Submission\ValueObject\SubmissionJudgeResult;
 use App\Domain\Submission\ValueObject\SubmissionType;
+use App\Domain\Submission\ValueObject\TestResultJudgeResult;
 
 class SubmissionTest extends TestCase
 {
-    protected $obj;
+    protected User $user;
+    protected Problem $problem;
+
     protected function setUp(): void
     {
-    }
-
-    public function test_success_submission()
-    {
         $language = Language::C;
-        $compileRuleDTOs = array (new CompileRuleFactoryDTO($language,'rightComand', 'rightComand' ));
-        $ExecutionRuleDTOs = array (new ExecutionRuleFactoryDTO($language, 'right', 'right', 'right', 'right'));
-        $TestcaseDTOs = array (new TestcasefactoryDTO('rightComand',$ExecutionRuleDTOs ));
-        $obj1 = User::Create('bavoiub', 'baviuhvfeqir', false);
-        $obj2 = Problem::Create('test', 'testbody', 130, 150, $compileRuleDTOs, $TestcaseDTOs);
-        $this->assertInstanceOf(Submission::class, Submission::Create($obj1, $obj2, $language, SubmissionType::SourceCode, 100));
+        $compileRuleDTOs = array(new CompileRuleFactoryDTO($language, 'rightComand', 'rightComand'));
+        $TestCaseDTOs = [
+            new TestCaseFactoryDTO("TestCase #1", [
+                new ExecutionRuleFactoryDTO($language, "", "", "", "")
+            ]),
+            new TestCaseFactoryDTO("TestCase #2", [
+                new ExecutionRuleFactoryDTO($language, "", "", "", "")
+            ]),
+            new TestCaseFactoryDTO("TestCase #3", [
+                new ExecutionRuleFactoryDTO($language, "", "", "", "")
+            ]),
+            new TestCaseFactoryDTO("TestCase #4", [
+                new ExecutionRuleFactoryDTO($language, "", "", "", "")
+            ]),
+            new TestCaseFactoryDTO("TestCase #5", [
+                new ExecutionRuleFactoryDTO($language, "", "", "", "")
+            ])
+        ];
+        $this->user = User::Create('bavoiub', 'baviuhvfeqir', false);
+        $this->problem = Problem::Create('test', 'testbody', 100, 100, $compileRuleDTOs, $TestCaseDTOs);
     }
 
-    public function test_success_testresult()
+    public function test_submission_creation()
     {
-        $this->assertInstanceOf(TestResult::class, TestResult::compar));
+        $submission = Submission::Create($this->user, $this->problem, Language::C, SubmissionType::SourceCode, 100);
+
+        $this->assertEquals(SubmissionJudgeResult::WJ, $submission->getJudgeResult());
+        $this->assertEquals(null, $submission->getExecutionTime());
+        $this->assertEquals(null, $submission->getConsumedMemory());
     }
 
-    public function test_invalid_testresult_judge()
+    public function test_createTestResult()
     {
-        $submission = new Submission();
-        $submission->getJudgeResult();
+        $submission = Submission::Create($this->user, $this->problem, Language::C, SubmissionType::SourceCode, 100);
 
+        $testCases = $this->problem->getTestCases();
+        $timeConstraint = $this->problem->getTimeConstraint();
+        $memoryConstraint = $this->problem->getMemoryConstraint();
+
+        // AC
+        $submission->createTestResult($this->problem, $testCases[0]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        $this->assertEquals(TestResultJudgeResult::AC, $submission->getTestResults()[0]->getJudgeResult());
+
+        // WA
+        $submission->createTestResult($this->problem, $testCases[1]->getId(), true, false, $timeConstraint - 1, $memoryConstraint - 1);
+        $this->assertEquals(TestResultJudgeResult::WA, $submission->getTestResults()[1]->getJudgeResult());
+
+        // RE
+        $submission->createTestResult($this->problem, $testCases[2]->getId(), false, true, $timeConstraint - 1, $memoryConstraint - 1);
+        $this->assertEquals(TestResultJudgeResult::RE, $submission->getTestResults()[2]->getJudgeResult());
+
+        // TLE
+        $submission->createTestResult($this->problem, $testCases[3]->getId(), false, false, $timeConstraint + 1, $memoryConstraint - 1);
+        $this->assertEquals(TestResultJudgeResult::TLE, $submission->getTestResults()[3]->getJudgeResult());
+
+        // MLE
+        $submission->createTestResult($this->problem, $testCases[4]->getId(), false, false, $timeConstraint - 1, $memoryConstraint + 1);
+        $this->assertEquals(TestResultJudgeResult::MLE, $submission->getTestResults()[4]->getJudgeResult());
     }
 
-    public function test_invalid_username_character()
+    public function test_judge_result_priority()
     {
-        $this->expectException(AlreadyJudgedException::class);
+        $submission = Submission::Create($this->user, $this->problem, Language::C, SubmissionType::SourceCode, 100);
 
-        User::Create('ああああああ', 'rightPassword', false);
-        User::Create('+-~\\{{}}[', 'rightPassword', false);
-        User::Create('];=::;<>?\\/', 'rightPassword', false);
-        User::Create('!@#$%^&*', 'rightPassword', false);
-    }
-    public function test_invalid_username_length()
-    {
-        $this->expectException(InvalidUsernameException::class);
-        User::Create('san', 'rightPassword', false);
-        User::Create('nizyuumozi01234567890', 'rightPassword', false);
-    }
+        $testCases = $this->problem->getTestCases();
+        $timeConstraint = $this->problem->getTimeConstraint();
+        $memoryConstraint = $this->problem->getMemoryConstraint();
 
-    public function test_invalid_password_character()
-    {
-        $this->expectException(InvalidPasswordException::class);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[0]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // WA
+        $submission->createTestResult($this->problem, $testCases[1]->getId(), true, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // RE
+        $submission->createTestResult($this->problem, $testCases[2]->getId(), false, true, $timeConstraint - 1, $memoryConstraint - 1);
+        // TLE
+        $submission->createTestResult($this->problem, $testCases[3]->getId(), false, false, $timeConstraint + 1, $memoryConstraint - 1);
+        // MLE
+        $submission->createTestResult($this->problem, $testCases[4]->getId(), false, false, $timeConstraint - 1, $memoryConstraint + 1);
 
-        User::Create('rightname', '全角はちもじいじょう', false);
-        User::Create('rightname', '+-~\\{{}}[];=::;<>?\\/', false);
-    }
+        $submission->completeJudge();
+        $this->assertEquals(SubmissionJudgeResult::MLE, $submission->getJudgeResult());
 
-    public function test_invalid_password_length()
-    {
-        $this->expectException(InvalidPasswordException::class);
 
-        User::Create('rightname', 'hatimoz', false);
-        User::Create('rightname', 'sanzyuumozi17697516974516943548549849849498949887512409', false);
+        $submission = Submission::Create($this->user, $this->problem, Language::C, SubmissionType::SourceCode, 100);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[0]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // WA
+        $submission->createTestResult($this->problem, $testCases[1]->getId(), true, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // RE
+        $submission->createTestResult($this->problem, $testCases[2]->getId(), false, true, $timeConstraint - 1, $memoryConstraint - 1);
+        // TLE
+        $submission->createTestResult($this->problem, $testCases[3]->getId(), false, false, $timeConstraint + 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[4]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+
+        $submission->completeJudge();
+        $this->assertEquals(SubmissionJudgeResult::TLE, $submission->getJudgeResult());
+
+
+        $submission = Submission::Create($this->user, $this->problem, Language::C, SubmissionType::SourceCode, 100);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[0]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // WA
+        $submission->createTestResult($this->problem, $testCases[1]->getId(), true, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // RE
+        $submission->createTestResult($this->problem, $testCases[2]->getId(), false, true, $timeConstraint - 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[3]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[4]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+
+        $submission->completeJudge();
+        $this->assertEquals(SubmissionJudgeResult::RE, $submission->getJudgeResult());
+
+
+        $submission = Submission::Create($this->user, $this->problem, Language::C, SubmissionType::SourceCode, 100);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[0]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // WA
+        $submission->createTestResult($this->problem, $testCases[1]->getId(), true, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[2]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[3]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[4]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+
+        $submission->completeJudge();
+        $this->assertEquals(SubmissionJudgeResult::WA, $submission->getJudgeResult());
+
+
+        $submission = Submission::Create($this->user, $this->problem, Language::C, SubmissionType::SourceCode, 100);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[0]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[1]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[2]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[3]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+        // AC
+        $submission->createTestResult($this->problem, $testCases[4]->getId(), false, false, $timeConstraint - 1, $memoryConstraint - 1);
+
+        $submission->completeJudge();
+        $this->assertEquals(SubmissionJudgeResult::AC, $submission->getJudgeResult());
     }
 }
