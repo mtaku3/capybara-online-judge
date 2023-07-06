c5a37cfd53740f65b1c4973df67c15b0c1e674cb
NaMLZo <nakat770@gmail.com>
2023-07-06 23:58:58 +0900

    test: MockFileRepositoryの作成

---
+++ tests/_files/Infrastructure/File/MockFileRepository.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Infrastructure\file;
+
+use App\Domain\file\IFileRepository;
+use App\Domain\file\Entity\file;
+use App\Domain\Problem\Entity\TestCase;
+use App\Domain\Submission\Entity\SourceFile;
+use App\Domain\Submission\Entity\Submission;
+use App\Infrastructure\Repository\file\Exception\FileMustBeArchivedAsTarballException;
+
+class MockFileRepository implements IFileRepository
+{
+    /**
+     * @var File[]
+     */
+    private array $records = [];
+
+    /**
+     * @param string $src
+     * @param Submission $submission
+     * @return void
+     */
+    public function moveSourceCode(string $src, Submission $submission): void
+    {
+        
+    }
+
+    /**
+     * @param string $src
+     * @param TestCase $testCase
+     * @return void
+     */
+    public function moveInputFile(string $src, TestCase $testCase): void
+    {  
+        
+    }
+
+    /**
+     * @param string $src
+     * @param TestCase $testCase
+     * @return void
+     */
+    public function moveOutputFile(string $src, TestCase $testCase): void
+    {
+        
+    }
+
+    /**
+     * @param string $src
+     * @return void
+     */
+    public static function ValidateTarball(string $src): void
+    {
+
+    }
+
+    /**
+     * @param string $src
+     * @return int
+     */
+    public static function SumContentLengthsUp(string $src): int
+    {
+        return 1;
+    }
+
+    /**
+     * @param SourceFile $sourceFile
+     * @return void
+     */
+    public function deleteSourceFile(SourceFile $sourceFile): void
+    {
+
+    }
+}
